2542a6d57695e03150d352d590ebcf555bd9d6f3
mani3354488 <mani3354488@gmail.com>
2021-04-17 19:12:42 +0530

    save script changes

--- dist/dbconfig/dbconfig.js
+++ dist/dbconfig/dbconfig.js
@@ -44,11 +44,11 @@ var pg_1 = require("pg");
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 var pool = new pg_1.Pool({
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
+    user: process.env.POSTGRESQL_ADDON_USER,
+    host: process.env.POSTGRESQL_ADDON_HOST,
+    database: process.env.POSTGRESQL_ADDON_DB,
+    password: process.env.POSTGRESQL_ADDON_PASSWORD,
+    port: 5432,
 });
 pool.on("connect", function (client) {
     console.log("client connection has been established");
--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
+    user: 'wwbhwcdefchkqjjecxb',
+    host: 'bjdsvullqjtg1ihjxa3s-postgresql.services.clever-cloud.com',
+    database: 'bjdsvullqjtg1ihjxa3s',
+    password: '4NNt3CN1fWPzj5rXUk0O',
+    port: 5432,
 });
 
 pool.on("connect", (client) => {
