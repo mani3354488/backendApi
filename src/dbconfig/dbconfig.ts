085c07fb78cdf93a5d4f59e9f81cad093190edfd
mani3354488 <mani3354488@gmail.com>
2021-04-17 20:21:56 +0530

    back to db1

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-    user: 'uj0makxizfh5usbpujoi',
-    host: 'b753jpx78siil6d92hnf-postgresql.services.clever-cloud.com',
-    database: 'b753jpx78siil6d92hnf',
-    password: 'x7fzL00DdYc6NUOKqT1C',
+    user: 'uwwbhwcdefchkqjjecxb',
+    host: 'bjdsvullqjtg1ihjxa3s-postgresql.services.clever-cloud.com',
+    database: 'bjdsvullqjtg1ihjxa3s',
+    password: '4NNt3CN1fWPzj5rXUk0O',
     port: 5432,
 });
 
