aaab106877c6b2e0fbad82dcadd145f1b541f10c
mani3354488 <mani3354488@gmail.com>
2021-04-17 14:01:11 +0530

    db connection details update

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -6,11 +7,13 @@ import BankRouter from "./routers/queryBanks";
 
 const app = express();
 
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.json({message: "api is working"});
 });
 
-app.use('/api/branches/', BankRouter);
+// app.use('/api/branches/', BankRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running at port ${process.env.PORT}`)
